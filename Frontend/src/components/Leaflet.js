369ada8515d4dd39ea1d9257c3dcdfee07f5dec8
Samuel Osei Adu <asamuel355@yahoo.com>
2022-11-22 13:35:10 +0000

    polygon inner label

--- Frontend/src/components/Leaflet.js
+++ Frontend/src/components/Leaflet.js
@@ -3,6 +3,7 @@ import { MapContainer, TileLayer, Popup, Polygon, } from 'react-leaflet'
 import "leaflet/dist/leaflet.css";
 import {useDispatch, useSelector} from 'react-redux'
 import { getPlots } from '../redux/features/PlotSlice';
+import PlotInner from './PlotInner';
 
 const Leaflet = () => {
 
@@ -25,6 +26,7 @@ const Leaflet = () => {
             {
                 plots && plots.map((plot) => (
                     <Polygon key={plot._id} positions={plot.geometry.coordinates} fillOpacity={0.8} fillColor="red" pathOptions={{color:'blue'}}>
+                        <PlotInner coord={plot.geometry.coordinates} plot_number={plot.properties.Plot_No} />   
                         <Popup>
                             {`Plot Number: ${plot.properties?.Plot_No} ${plot.properties?.Street_Nam}`}
                             <button></button>
