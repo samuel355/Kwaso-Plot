6865915f717a0844f0e5517251c2442aa4dd2564
Samuel Osei Adu <asamuel355@yahoo.com>
2022-11-23 17:34:49 +0000

    tailwind header

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -1,55 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import {Link} from 'react-router-dom'
 import { logout } from '../redux/features/UserSlice'
 
 const Header = () => {
+    const [dropdown, setDropdown] = useState(false)
+    const [menu, setMenu] = useState(false)
     const dispatch = useDispatch()
     const {user, loading, error} = useSelector((state) => ({...state.user}))
     return (
         <>
-            <header className="header_area bg-dark" style={{opacity: 0.5, zIndex: 999}} id="header">
-                <div className="container-fluid h-100">
-                    <div className="row h-100">
-                        <div className="col-12 h-100">
-                            <nav className="h-100 navbar navbar-expand-lg align-items-center">
-                                <a className="navbar-brand" href="/">L.H.C.</a>
-                                <button style={{border: 'none'}} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#fancyNav" aria-controls="fancyNav" aria-expanded="false" aria-label="Toggle navigation"> 
-                                    <i className=" text-white fa fa-duotone fa-bars"></i>
-                                </button>
-                                <div className="collapse navbar-collapse" id="fancyNav">
-                                    <ul className="navbar-nav ml-auto">
-                                        <li className="nav-item">
-                                            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-                                        </li>
-                                        <li className="nav-item">
-                                            <a className="nav-link" href="services.php">Services</a>
-                                        </li>
-                                        <li className="nav-item">
-                                            <a className="nav-link" href="plots.php">Our Plots</a>
-                                        </li>
-                                        <li className="nav-item dropdown">
-                                            <a className="nav-link dropdown-toggle" href="#1" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Projects</a>
-                                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                                <a className="dropdown-item" href="#2">Dar ES Salaam</a>
-                                                <a className="dropdown-item" href="#3">Gratia Hills</a>
-                                                <a className="dropdown-item" href="#4">Diaspora Dream Project</a>
-                                            </div>
-                                        </li>
-                                        {
-                                            user && (
-                                                <li className="nav-item" onClick={() => dispatch(logout())}>
-                                                    <Link className="nav-link">Logout</Link>
-                                                </li>
-                                            )
-                                        }
-                                    </ul>
-                                </div>
-                            </nav>
+            <nav class="bg-gray-800 fixed-top">
+                <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                    <div class="relative flex h-16 items-center justify-between">
+                    <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                        {/* <!-- Mobile menu button--> */}
+                        <button onClick={() => setMenu(!menu)} type="button" class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                            <span class="sr-only">Open main menu</span>
+                            
+                            <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                            </svg>
+
+                            <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                        <div class="flex flex-shrink-0 items-center">
+                            <img class="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+                            <img class="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+                        </div>
+                        <div class="hidden sm:ml-6 sm:block">
+                            <div class="flex space-x-4">
+                                <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
+
+                                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
+
+                                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
+
+                                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
+                            </div>
                         </div>
                     </div>
+                    <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                        <div class="relative ml-3">
+                            <div>
+                                <button onClick={() => setDropdown(!dropdown)} type="button" class="flex text-white rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                    name 
+                                </button>
+                            </div>
+                            {
+                                dropdown && (
+                                    <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                                        <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
+                                        <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
+                                        <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+                                    </div>
+                                )
+                            }
+                        </div>
+                    </div>
+                    </div>
                 </div>
-            </header>
+
+                {
+                    menu && (
+                        <div class="sm:hidden transition ease-out duration-100" id="mobile-menu">
+                            <div class="space-y-1 px-2 pt-2 pb-3">
+                            
+                                <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
+
+                                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
+
+                                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
+
+                                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+                            </div>
+                        </div>
+                    )
+                }
+            </nav>
+
             <div className="fancy-breadcumb-area bg-img bg-overlay" style={{backgroundImage: 'url("/25.jpg")'}}>
                 <div className="container h-100">
                     <div className="row h-100 align-items-center">
